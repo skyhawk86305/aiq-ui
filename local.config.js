8ab756a815e7945ac7f53ddcab6d737937614592
Nathan Hammernik <nathan.hammernik@solidfire.com>
2016-06-27 17:23:12 +0000

    local.config.js deleted online with Bitbucket

--- local.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  host: 'localhost',
-  port: 3000,
-  apiServer: 'http://activeiq.dev.aiq.solidfire.net',
-  username: 'nathan.hammernik@solidfire.com',
-  password: 'Nhammernik28'
-}
