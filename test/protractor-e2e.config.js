1109bac7f42407ed42e9d160b3c739f47d5ca082
Nathan Hammernik <nathan.hammernik@solidfire.com>
2016-06-28 18:00:51 -0600

    CLOUD-2893 - Add reporting for e2e tests

--- test/protractor-e2e.config.js
+++ test/protractor-e2e.config.js
@@ -14,7 +14,13 @@ exports.config = {
 
   onPrepare: function() {
     var SpecReporter = require('jasmine-spec-reporter');
+    var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
     jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
+    jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
+      savePath: './report/e2e/',
+      takeScreenshots: true,
+      takeScreenshotsOnlyOnFailures: true
+    }));
     browser.driver.manage().window().maximize();
   },
 
