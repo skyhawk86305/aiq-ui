5b9750d78d314ba3a3c5e3d8d17f44321599ebff
Steve Roy <stephen.roy@netapp.com>
2016-11-02 13:45:39 -0600

    Created seperate describe for these tests and incorporated with what is now in master

--- test/e2e/authentication/authentication.spec.js
+++ test/e2e/authentication/authentication.spec.js
@@ -1,35 +1,31 @@
 /* jshint expr: true */
 'use strict';
 
+var expect = require('../support.js').expect;
+var mockBackend = require('../support.js').mockBackend;
 var support = require('../support.js');
-var expect = support.expect;
 var LoginComponent = require('../../page-objects/login.po');
 var NavbarComponent = require('../../page-objects/navbar.po');
-var request = require('request');
 
-var navbar = new NavbarComponent();
 var loginForm = new LoginComponent();
-
-var logout = function(callback) {
-  request.delete(browser.baseUrl + '/sessions', callback());
-};
-
-var login = function(callback) {
-  var params = {username: 'testuser@solidfire.com', password: new Buffer('password123').toString('base64')};
-  return request({
-    method: 'PUT',
-    uri: browser.baseUrl + '/sessions',
-    json: true,
-    body: params
-  }, callback());
-};
+var navbar = new NavbarComponent();
 
 describe('Login Form', function() {
-  beforeEach(function(done) {
-    logout(function() {
-      browser.get('#');
-      done();
+  beforeEach(function() {
+    mockBackend.enable(browser);
+    mockBackend.http.whenGET('/sessions').respond(function() {
+      return [400, {}];
     });
+    browser.get('#').then(function() {
+      // Sleep statement currently needed to get test to pass on Jenkins.
+      // Further investigation is needed to see if we can find the root
+      // of the problem.
+      browser.driver.sleep(200);
+    });
+  });
+
+  afterEach(function() {
+    mockBackend.disable();
   });
 
   it('should not let me click the login button if the fields are empty', function() {
@@ -51,9 +47,47 @@ describe('Login Form', function() {
     loginForm.passwordInput.enter('Password123');
     expect(loginForm.loginButton.el.isEnabled()).to.eventually.be.true;
   });
+});
 
-  it('should have a password input field type of password', function () {
-    expect(loginForm.passwordInput.el.getAttribute('type')).to.eventually.equal('password');
+describe('Navigation with Authentication', function() {
+  beforeEach(function() {
+    mockBackend.enable(browser);
+    mockBackend.http.whenPOST('/v2/api').passThrough();
+  });
+
+  afterEach(function() {
+    mockBackend.disable();
+  });
+
+  it('should redirect me to the login page if my aiq session is invalid when I try to navigate to another page', function() {
+    // Login to site.
+    var authMock = mockBackend.http.whenGET('/sessions');
+    authMock.respond(function() {
+      return [200, {}];
+    });
+
+    browser.get('#/dashboard/overview').then(function() {
+      expect(browser.getLocationAbsUrl()).to.eventually.contain('/dashboard/overview');
+
+      mockBackend.http.reset();
+      authMock.respond(function() {
+        return [400, {}];
+      });
+
+      browser.get('#/dashboard/health');
+      expect(browser.getLocationAbsUrl()).to.eventually.contain('/login');
+    });
+  });
+
+  it('should not let me navigate to an aiq page via URL if I\'m not unauthenticated', function() {
+    browser.get('#/dashboard/overview');
+    expect(browser.getLocationAbsUrl()).to.eventually.contain('/login');
+  });
+});
+
+describe('Input default and focus', function() {
+  beforeEach(function() {
+    browser.get('#/login');
   });
 
   it('should have a default submit', function () {
@@ -62,43 +96,11 @@ describe('Login Form', function() {
     loginForm.el.submit();
     expect(navbar.el.isPresent()).to.eventually.be.true;
   });
-});
-
-describe('Login form input field focus', function() {
-  beforeEach(function(done) {
-    logout(function() {
-      browser.get('#');
-      done();
-    });
-  });
 
   it('should get focus when selected', function () {
     loginForm.usernameInput.click();
     expect(support.getActiveElement().getAttribute('id')).to.eventually.equal('username-input');
     loginForm.passwordInput.click();
     expect(support.getActiveElement().getAttribute('id')).to.eventually.equal('password-input');
-  });
-});
-
-describe('Navigation with Authentication', function() {
-  afterEach(function(done) {
-    login(done);
-  });
-
-  it('should not let me navigate to an aiq page via URL if I\'m not unauthenticated', function(done) {
-    logout(done);
-    browser.get('#/dashboard');
-    expect(browser.getLocationAbsUrl()).to.eventually.contain('/login');
-  });
-
-  it('should redirect me to the login page if my aiq session is invalid when I try to navigate to another page', function(done) {
-    browser.get('#');
-    navbar.subNavbar.click('dashboard-health').then(function() {
-      logout(function() {
-        navbar.mainNavbar.click('dashboard');
-        done();
-      });
-    });
-    expect(browser.getLocationAbsUrl()).to.eventually.contain('/login');
   });
 });
