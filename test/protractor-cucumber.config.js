1321f2f4b0b0d2c8d79f4e6a50a183b0e8a30f27
Steve Roy <stephen.roy@netapp.com>
2016-11-02 09:34:42 -0600

    Removed my changes

--- test/protractor-cucumber.config.js
+++ test/protractor-cucumber.config.js
@@ -3,10 +3,6 @@ exports.config = {
   framework: 'custom',
   frameworkPath: require.resolve('protractor-cucumber-framework'),
 
-  seleniumAddress:"http://127.0.0.1:4444/wd/hub",
-
-  baseUrl:"http://localhost:3001",
-
   allScriptsTimeout: 60000,
 
   capabilities: {
