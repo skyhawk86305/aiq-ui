463216c22ffe7d65457ef259acac6a222ced77e9
jennifer.roos@netapp.com <jennifer.roos@netapp.com>
2016-11-16 08:24:47 -0700

    CLOUD-3142 - added spec for cluster overview e2e test

--- test/e2e/cluster/reporting/overview.spec.js
+++ test/e2e/cluster/reporting/overview.spec.js
@@ -32,4 +32,42 @@ describe('Cluster Overview Page', function () {
     mockBackend.disable();
   });
 
+  it('should have the performance graph', function () {
+    expect(performancePage.graphs.clusterPerformance.isDisplayed()).to.eventually.be.true;
+  });
+
+  it('should have the utilization graph', function () {
+    expect(performancePage.graphs.performanceUtilization.isDisplayed()).to.eventually.be.true;
+  });
+
+  it('should display the infobar', function() {
+    expect(clusterOverviewPage.infoBar.isDisplayed()).to.eventually.be.true;
+  });
+
+  it('should have 8 badges', function() {
+    expect(clusterOverviewPage.infoBar.infoBoxes.count()).to.eventually.equal(8);
+  });
+
+  it('should have the correct badges', function() {
+    expect(clusterOverviewPage.infoBar.infoBox('node-count').box.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('block-capacity').box.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('metadata-capacity').box.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('efficiency').box.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('utilization').box.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('iops').box.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('bandwidth').box.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('cluster-faults').box.isDisplayed()).to.eventually.be.true;
+  });
+
+  it('should have the correct badges', function() {
+    expect(clusterOverviewPage.infoBar.infoBox('block-capacity').badge().el.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('metadata-capacity').badge().el.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('block-capacity').badge('first').el.isDisplayed()).to.eventually.be.true;
+    expect(clusterOverviewPage.infoBar.infoBox('metadata-capacity').badge('second').el.isDisplayed()).to.eventually.be.true;
+  });
+
+  it('should have the alerts table', function () {
+    expect(performancePage.alertTable.isDisplayed()).to.eventually.be.true;
+  });
+
 });
