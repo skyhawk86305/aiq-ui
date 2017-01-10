d2569048c9daeac5b92cb57b477c63ae59e8fc57
Vania Xu <vania.xu@solidfire.com>
2017-01-10 14:41:37 -0700

    Added export functionality to all aiq tables

--- app/dashboard/alert/policies/alert-policies-table.component.ts
+++ app/dashboard/alert/policies/alert-policies-table.component.ts
@@ -4,7 +4,7 @@
   angular
     .module('aiqUi')
     .component('alertPolicyTable', {
-      template: '<sf-table class="sf-layout-block" service="$ctrl.service" table-id="alert-policy" control-bar="true" items-per-page="25"></sf-table>',
+      template: '<sf-table class="sf-layout-block" service="$ctrl.service" table-id="alert-policy" control-bar="true" items-per-page="25" export="true"></sf-table>',
       controller: ['AlertPolicyTableService', AlertPolicyTableController]
     });
 
