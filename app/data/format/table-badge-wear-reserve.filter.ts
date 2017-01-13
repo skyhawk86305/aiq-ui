2e95dda715934d2adaa451b1cd43f6bde40a29b9
Brian Abbott <brian.abbott@netapp.com>
2017-01-13 12:55:45 -0500

    changed threshold for red badge to be less than 20

--- app/data/format/table-badge-wear-reserve.filter.ts
+++ app/data/format/table-badge-wear-reserve.filter.ts
@@ -7,7 +7,7 @@
       return function (data) {
         if (data > 50) {
           return '<div class="table-badge -normal">' + data + '</div>';
-        } if (data < 50 && data > 15) {
+        } if (data < 50 && data > 20) {
           return '<div class="table-badge -warning">' + data + '</div>';
         } else {
           return '<div class="table-badge -critical">' + data + '</div>';
