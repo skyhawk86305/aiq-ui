21d7cc1d14b672f7439c60164ea8650e9ea48dde
Vania Xu <vania.xu@solidfire.com>
2017-01-19 15:51:57 -0700

    added quick filter to drives table

--- app/cluster/drives/drive-table.component.ts
+++ app/cluster/drives/drive-table.component.ts
@@ -4,12 +4,22 @@
   angular
     .module('aiqUi')
     .component('driveTable', {
-      template: '<sf-table class="sf-layout-block" service="$ctrl.service" table-id="drive" control-bar="true" items-per-page="25" export="true"></sf-table>',
+      template: '<sf-table class="sf-layout-block" service="$ctrl.service" table-id="drive" control-bar="true" items-per-page="25" quick-filter="$ctrl.quickFilter" export="true"></sf-table>',
       controller: ['$routeParams', 'DriveTableService', DriveTableController]
     });
 
   function DriveTableController($routeParams, DriveTableService) {
     this.service = DriveTableService;
     this.service.update($routeParams.clusterID);
+    this.quickFilter = {
+      column: 'status',
+      values: ['active', 'available', 'failed'],
+      display: {
+        active: 'Active',
+        available: 'Available',
+        failed: 'Failed'
+      },
+      default: 'active'
+    };
   }
 })();
