c1151dfa6913f4ff7250616696712ba99d7e0c7f
Brian Abbott <brian.abbott@netapp.com>
2017-01-20 12:38:25 -0500

    fixed else if statements

--- app/data/format/table-badge-reserve.filter.ts
+++ app/data/format/table-badge-reserve.filter.ts
@@ -5,9 +5,9 @@
     .module('aiqUi')
     .filter('tableBadgeReserve', function() {
       return function (data) {
-        if (data < 20) {
+        if (data < 1) {
           return '<div class="table-badge -critical">' + data + '</div>';
-        } if (data < 80) {
+        } else if (data < 20) {
           return '<div class="table-badge -warning">' + data + '</div>';
         } else {
           return '<div class="table-badge -normal">' + data + '</div>';
--- app/data/format/table-badge-wear.filter.ts
+++ app/data/format/table-badge-wear.filter.ts
@@ -7,7 +7,7 @@
       return function (data) {
         if (data < 10) {
           return '<div class="table-badge -critical">' + data + '</div>';
-        } if (data < 80) {
+        } else if (data < 80) {
           return '<div class="table-badge -warning">' + data + '</div>';
         } else {
           return '<div class="table-badge -normal">' + data + '</div>';
