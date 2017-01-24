4133a0455fbb3c6c7db4bfdb6ef66d075b3c1cdf
Vania Xu <vania.xu@solidfire.com>
2017-01-24 14:04:15 -0700

    e2e test clean up

--- test/e2e/cluster/virtual-volumes/virtual-volumes.spec.js
+++ test/e2e/cluster/virtual-volumes/virtual-volumes.spec.js
@@ -11,18 +11,6 @@ var itemsPerPage = 25;
 var maxRows = fixture.length > itemsPerPage ? itemsPerPage : fixture.length;
 var columns = [
   {key: 'volumeID', label: 'ID', format: {filter: 'string'}},
-  {key: 'accountID', label: 'Account ID', format: {filter: 'string'}},
-  {key: 'totalSize', label: 'Volume Size', format: {filter: 'bytes'}},
-  {key: 'enable512e', label: '512e', format: {filter: 'boolean', args: ['Yes', 'No']}},
-  {key: 'access', label: 'Access', format: {filter: 'string'}},
-  {key: 'minIOPS', label: 'Min IOPS', format: {filter: 'aiqNumber', args: [0, false, true]}},
-  {key: 'maxIOPS', label: 'Max IOPS', format: {filter: 'aiqNumber', args: [0, false, true]}},
-  {key: 'burstIOPS', label: 'Burst IOPS', format: {filter: 'aiqNumber', args: [0, false, true]}},
-  {key: 'paired', label: 'Paired', format: {filter: 'boolean', args: ['Yes', 'No']}},
-  {key: 'configuredAccessProtocols', label: 'Configured Access Protocols', format: {filter: 'string'}}
-];
-var columns = [
-  {key: 'volumeID', label: 'ID', format: {filter: 'string'}},
   {key: 'snapshotID', label: 'Snapshot ID', format: {filter: 'string'}},
   {key: 'parentVirtualVolumeID', label: 'Parent Virtual Volume ID', format: {filter: 'string'}},
   {key: 'virtualVolumeID', label: 'Virtual Volume ID', format: {filter: 'string'}},
