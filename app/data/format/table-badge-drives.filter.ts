eba3503d0682a547a979944c4cb2df609687bd0b
Brian Abbott <brian.abbott@netapp.com>
2017-01-25 17:31:16 -0500

    simplified checks for non-number data, cleaned up logical statements

--- app/data/format/table-badge-drives.filter.ts
+++ app/data/format/table-badge-drives.filter.ts
@@ -6,30 +6,19 @@
     .filter('drivesTableBadge', ['$filter', function($filter) {
       return function (data, column) {
 
-        if (data === null || data === undefined || data === '') {
+        if (typeof data !== 'number') {
           return '<div class="table-badge -empty">-</div>';                
         }
 
-        if (typeof(data) === 'boolean') {
-          return $filter('tableBadgeBoolean')(data);
-        }
+        var criticalThreshold = column === 'wear' ? 10 : 1;
+        var warningThreshold = column === 'wear' ? 80 : 20;
 
         var type = 'normal';
 
-        if (column === 'reserve') {
-          if (data < 1) {
-            type = 'critical';
-          } else if (data < 20) {
-            type = 'warning';
-          }
-        }
-
-        if (column === 'wear') {
-          if (data < 10) {
-            type = 'critical'
-          } else if (data < 80) {
-            type = 'warning';
-          }
+        if (data < criticalThreshold) {
+          type = 'critical';
+        } else if (data < warningThreshold) {
+          type = 'warning';
         }
 
         return '<div class="table-badge -' + type + '">' + data + '</div>';
--- test/unit/data/format/drives-table-badge.filter.spec.ts
+++ test/unit/data/format/drives-table-badge.filter.spec.ts
@@ -8,9 +8,9 @@ describe('Table Badge Drives Filter', function () {
     expect($filter('drivesTableBadge')(null)).toEqual('<div class="table-badge -empty">-</div>');
     expect($filter('drivesTableBadge')(undefined)).toEqual('<div class="table-badge -empty">-</div>');
     expect($filter('drivesTableBadge')('')).toEqual('<div class="table-badge -empty">-</div>');
-
-    expect($filter('drivesTableBadge')(true)).toEqual('<div class="table-badge -normal">YES</div>');
-    expect($filter('drivesTableBadge')(false)).toEqual('<div class="table-badge -critical">NO</div>');
+    expect($filter('drivesTableBadge')(true)).toEqual('<div class="table-badge -empty">-</div>');
+    expect($filter('drivesTableBadge')(false)).toEqual('<div class="table-badge -empty">-</div>');
+    expect($filter('drivesTableBadge')('1000')).toEqual('<div class="table-badge -empty">-</div>');
 
     expect($filter('drivesTableBadge')(9, 'wear')).toEqual('<div class="table-badge -critical">9</div>');
     expect($filter('drivesTableBadge')(30, 'wear')).toEqual('<div class="table-badge -warning">30</div>');
@@ -19,8 +19,6 @@ describe('Table Badge Drives Filter', function () {
     expect($filter('drivesTableBadge')(0, 'reserve')).toEqual('<div class="table-badge -critical">0</div>');
     expect($filter('drivesTableBadge')(10, 'reserve')).toEqual('<div class="table-badge -warning">10</div>');
     expect($filter('drivesTableBadge')(90, 'reserve')).toEqual('<div class="table-badge -normal">90</div>');
-
-    expect($filter('drivesTableBadge')('1000')).toEqual('<div class="table-badge -normal">1000</div>');
   }));
 
 });
