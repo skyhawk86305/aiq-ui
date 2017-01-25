637393d206e7b32016c5d9df13b8af64c59d04b3
Brian Abbott <brian.abbott@netapp.com>
2017-01-25 17:36:02 -0500

    moved variable declarations to top of function

--- app/data/format/table-badge-drives.filter.ts
+++ app/data/format/table-badge-drives.filter.ts
@@ -5,15 +5,13 @@
     .module('aiqUi')
     .filter('drivesTableBadge', ['$filter', function($filter) {
       return function (data, column) {
+        var criticalThreshold = column === 'wear' ? 10 : 1;
+        var warningThreshold = column === 'wear' ? 80 : 20;
+        var type = 'normal';
 
         if (typeof data !== 'number') {
           return '<div class="table-badge -empty">-</div>';                
         }
-
-        var criticalThreshold = column === 'wear' ? 10 : 1;
-        var warningThreshold = column === 'wear' ? 80 : 20;
-
-        var type = 'normal';
 
         if (data < criticalThreshold) {
           type = 'critical';
