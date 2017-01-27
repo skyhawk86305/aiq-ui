03dada0df210ddaf40ade9fe4e51acf45a799c61
Sreymoch Don <sreymoch.don@netapp.com>
2017-01-27 11:35:48 -0700

    Add badges to alert history table and error log severity and resolved column

--- app/data/format/table-badge-alert-severity.filter.ts
+++ app/data/format/table-badge-alert-severity.filter.ts
@@ -1,6 +1,6 @@
 (function () {
   'use strict';
-  
+
   angular
     .module('aiqUi')
     .filter('tableBadgeAlertSeverity', function() {
@@ -13,6 +13,14 @@
           case 'Error':
             return '<div class="table-badge -error">ERROR</div>';
           case 'Critical':
+            return '<div class="table-badge -critical">CRITICAL</div>';
+          case 'info':
+            return '<div class="table-badge -info">INFO</div>';
+          case 'warning':
+            return '<div class="table-badge -warning">WARNING</div>';
+          case 'error':
+            return '<div class="table-badge -error">ERROR</div>';
+          case 'critical':
             return '<div class="table-badge -critical">CRITICAL</div>';
           default:
             return data;
