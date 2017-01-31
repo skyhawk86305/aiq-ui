1b283512c7c759d1aa101fb64d4bb9677fec4dd5
Nathan Hammernik <nathan.hammernik@solidfire.com>
2017-01-31 14:44:46 -0700

    Inline styles rather than loading from single file

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,5 +1,4 @@
 var webpack = require('webpack'),
-  ExtractTextPlugin = require('extract-text-webpack-plugin'),
   HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -14,14 +13,13 @@ module.exports = {
     loaders: [
       { test: /\.ts$/, loader: 'ts' },
       { test: /\.html$/, loader: 'raw' },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css?-url') },
-      { test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css!less') },
+      { test: /\.css$/, loader: 'style!css?-url' },
+      { test: /\.less$/, loader: 'style!css!less' },
       { test: /\.(png|jpe?g|gif|ico|svg|woff|woff2|ttf|eot)([\?]?.*)$/, loader: 'file?name=images/[name].[ext]' }
     ]
   },
   plugins: [
     new webpack.NoErrorsPlugin(),
-    new ExtractTextPlugin('[name].css'),
     new HtmlWebpackPlugin({ template: './app/index.html' }),
     new webpack.ProvidePlugin({ d3: 'd3' }) // ToDo: remove once sf-components is packaged correctly
   ]
