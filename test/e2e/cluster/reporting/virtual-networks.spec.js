aaddee8449636a1651890a1160b1a99400da0ccb
Nathan Hammernik <nathan.hammernik@solidfire.com>
2017-02-07 16:48:43 -0700

    CLOUD-3394 - Add e2e test for virtual networks table

---
+++ test/e2e/cluster/reporting/virtual-networks.spec.js
@@ -0,0 +1,49 @@
+/* jshint expr: true */
+'use strict';
+
+var support = require('../../support.js');
+var expect = support.expect;
+var TableComponent = require('../../page-objects/components/sf-components.po').table;
+var table = new TableComponent('virtual-network');
+var fixture = mapFixture(support.fixture('ListVirtualNetworks'));
+var uniqueKey = 'virtualNetworkID';
+var itemsPerPage = 25;
+var maxRows = fixture.length > itemsPerPage ? itemsPerPage : fixture.length;
+var columns = [
+  {key: 'virtualNetworkID', label: 'ID', format: {filter: 'aiqNumber', args: [0, true]}},
+  {key: 'name', label: 'Name', format: {filter: 'string'}},
+  {key: 'virtualNetworkTag', label: 'VLAN Tag', format: {filter: 'aiqNumber', args: [0, true]}},
+  {key: 'svip', label: 'SVIP', format: {filter: 'string'}},
+  {key: 'netmask', label: 'Netmask', format: {filter: 'string'}},
+  {key: 'gateway', label: 'Gateway', format: {filter: 'string'}},
+  {key: 'namespace', label: 'VRF Enabled', format: {filter: 'boolean', args:['Yes','No']}},
+  {key: 'addressBlocks', label: 'IPs Used', format: {filter: 'ipsUsed'}}
+];
+
+function mapFixture(rawFixture) {
+  return rawFixture.virtualNetworks.map(function(network) {
+    return network;
+  });
+}
+
+fdescribe('The Cluster Virtual Networks Page', function () {
+  it('should display a table component on page load', function () {
+    browser.get('#/cluster/26/reporting/virtualNetworks');
+    expect(table.el.isDisplayed()).to.eventually.be.true;
+  });
+
+  it('should have the correct columns and headers', function () {
+    expect(table.content.columns.count()).to.eventually.equal(columns.length);
+    columns.forEach(function(column) {
+      expect(table.content.header(column.key).title.getText()).to.eventually.equal(column.label);
+    });
+  });
+
+  it('should display data from the correct API and properly format it in the table', function (done) {
+    support.testTableData(table, columns, maxRows, uniqueKey, fixture, done);
+  });
+
+  it('should have an export button for the table', function() {
+    expect(table.controlBar.export.button.isPresent()).to.eventually.be.true;
+  });
+});
