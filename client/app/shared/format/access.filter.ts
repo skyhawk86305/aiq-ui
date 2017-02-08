7f10bdbac8d5bf6f980e22cc7bee30c2a6bf96a2
Brian Abbott <brian.abbott@netapp.com>
2017-02-08 10:53:55 -0500

    moved access.filter.spec.ts to correct folder, modified access.filter.ts NOTE: still not working, getting “Error: [$injector:unpr] Unknown provider: accessFilterProvider <- accessFilter” - still working on issue

--- client/app/shared/format/access.filter.ts
+++ client/app/shared/format/access.filter.ts
@@ -1,22 +1,18 @@
-(function () {
-  'use strict';
+export function AccessFilter($filter) {
+  return function (data) {
+    switch (data) {
+      case 'readWrite':
+        return 'Read / Write';
+      case 'readOnly':
+        return 'Read Only';
+      case 'locked':
+        return 'Locked';
+      case 'replicationTarget':
+        return 'Replication Target';
+      default:
+        return $filter('string')(data);
+    }
+  };
+}
 
-  angular
-    .module('aiqUi')
-    .filter('access', ['$filter', function($filter) {
-      return function (data) {
-        switch (data) {
-          case 'readWrite':
-            return 'Read / Write';
-          case 'readOnly':
-            return 'Read Only';
-          case 'locked':
-            return 'Locked';
-          case 'replicationTarget':
-            return 'Replication Target';
-          default:
-          	return $filter('string')(data);
-        }
-      };
-    }]);
-})();
+AccessFilter.$inject = ['$filter'];
