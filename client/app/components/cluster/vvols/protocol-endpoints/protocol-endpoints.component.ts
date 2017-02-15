baa75785bf871e54c8d0d38d7c3903c86f893a21
Brian Abbott <brian.abbott@netapp.com>
2017-02-15 11:43:54 -0500

    added footer-row-count to all tables with pagination in aiq-ui

--- client/app/components/cluster/vvols/protocol-endpoints/protocol-endpoints.component.ts
+++ client/app/components/cluster/vvols/protocol-endpoints/protocol-endpoints.component.ts
@@ -4,7 +4,7 @@
   angular
     .module('aiqUi')
     .component('protocolEndpointTable', {
-      template: '<sf-table class="sf-layout-block" service="$ctrl.service" table-id="protocol-endpoint" control-bar="true" column-selection="true" items-per-page="25" export="true"></sf-table>',
+      template: '<sf-table class="sf-layout-block" service="$ctrl.service" table-id="protocol-endpoint" control-bar="true" column-selection="true" items-per-page="25" export="true" footer-row-count="true"></sf-table>',
       controller: ['$routeParams', 'ProtocolEndpointTableService', ProtocolEndpointTableController]
     });
 
--- client/app/components/dashboard/alerts/history/history.component.ts
+++ client/app/components/dashboard/alerts/history/history.component.ts
@@ -5,7 +5,7 @@
     .module('aiqUi')
     .component('alertHistoryTable', {
       template: '<sf-table class="sf-layout-block" service="$ctrl.service" table-id="alert-history" ' +
-        'multi-column-sort="true" control-bar="true" items-per-page="25" export="true"></sf-table>',
+        'multi-column-sort="true" control-bar="true" items-per-page="25" export="true" footer-row-count="true"></sf-table>',
       controller: ['AlertHistoryTableService', AlertHistoryTableController]
     });
 
