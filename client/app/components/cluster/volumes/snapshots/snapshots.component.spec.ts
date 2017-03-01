e1326477caa05f8a510fda58a9167d5a7d67a2c4
Sreymoch Don <sreymoch.don@netapp.com>
2017-03-01 16:50:28 -0700

    Add component unit test

---
+++ client/app/components/cluster/volumes/snapshots/snapshots.component.spec.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+fdescribe('Component: snapshotTable', function() {
+  let routeParams,
+    service,
+    controller;
+
+  beforeEach(angular.mock.module('aiqUi'));
+
+  beforeEach(inject(function($componentController, $routeParams, SnapshotTableService) {
+    routeParams = $routeParams;
+    routeParams.clusterID = 'foobar';
+    service = SnapshotTableService;
+    spyOn(service, 'update');
+    controller = $componentController('snapshotTable');
+  }));
+
+  describe('snapshot table initialization', function() {
+    it('should expose the table service to be used in the sf-table component', function() {
+      expect(controller.service).toEqual(service);
+    });
+
+    it('should update the table service with the clusterID and volumeID from the route', function() {
+
+    });
+  });
+});
