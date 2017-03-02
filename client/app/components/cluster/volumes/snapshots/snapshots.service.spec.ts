bb737fadb56a6dbbbeaed24fcdfc6976b98590bb
Sreymoch Don <sreymoch.don@netapp.com>
2017-03-02 08:55:31 -0700

    add snapshot service spec

--- client/app/components/cluster/volumes/snapshots/snapshots.service.spec.ts
+++ client/app/components/cluster/volumes/snapshots/snapshots.service.spec.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-fdescribe('service: SnapshotTableService', function() {
+describe('service: SnapshotTableService', function() {
   let service,
     dataService,
     deferred,
@@ -79,8 +79,43 @@ fdescribe('service: SnapshotTableService', function() {
       rootScope.$apply();
     });
 
-    it('should expect no snapshots', function() {
+    it('should return no snapshots when no volume associated with any snapshot', function() {
+      service.volumeID = 31;
+      apiResponse = {
+        volumes: [{volumeID: 33, accountID: 2, totalSize: 500000882688, status: 'active'}, {volumeID: 31, accountID: 1, totalSize: 500000882688, status: 'active'}],
+        snapshots: [{snapshotID: 20, volumeID: 33, totalSize: 500000882688, expirationTime: null, snapshotUUID: '61573ec7-3ffe-43f6-8a0a-f7d87da384d4', enableRemoteReplication: false, groupID: 0, createTime: '2016-05-18T21:36:24Z'},
+          {snapshotID: 21, volumeID: 30, totalSize: 500000882688, expirationTime: null, snapshotUUID: '61573ec7-3ffe-43f6-8a0a-f7d87da38422', enableRemoteReplication: true, groupID: 1, createTime: '2016-04-18T21:36:24Z'}]
+      }
+      deserializedResponse = [];
+      service.getData(true).then(function(response) {
+        expect(response).toEqual(deserializedResponse);
+      });
+      deferred.resolve(apiResponse);
+      rootScope.$apply();
+    });
 
+    it('should return no snapshots when selected volume does not exists', function() {
+      service.volumeID = 11;
+      apiResponse = {
+        volumes: [{volumeID: 33, accountID: 2, totalSize: 500000882688, status: 'active'}, {volumeID: 31, accountID: 1, totalSize: 500000882688, status: 'active'}],
+        snapshots: [{snapshotID: 20, volumeID: 33, totalSize: 500000882688, expirationTime: null, snapshotUUID: '61573ec7-3ffe-43f6-8a0a-f7d87da384d4', enableRemoteReplication: false, groupID: 0, createTime: '2016-05-18T21:36:24Z'},
+          {snapshotID: 21, volumeID: 30, totalSize: 500000882688, expirationTime: null, snapshotUUID: '61573ec7-3ffe-43f6-8a0a-f7d87da38422', enableRemoteReplication: true, groupID: 1, createTime: '2016-04-18T21:36:24Z'}]
+      }
+      service.getData(true).then(function(response) {
+        expect(response).toBeUndefined();
+      });
+      deferred.resolve(apiResponse);
+      rootScope.$apply();
+    });
+
+    it('should reject the error message if the call fails', function() {
+
+      let apiFailure = 'FooError';
+      service.getData(true).catch(function(err) {
+        expect(err).toEqual(apiFailure);
+      });
+      deferred.reject(apiFailure);
+      rootScope.$apply();
     });
   });
 });
