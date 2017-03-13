ab850f8f5a6b0cc77f264730e6eb79b0e1020c23
Trey Thomas <trey.thomas@netapp.com>
2017-03-13 13:41:19 -0600

    Remove one-hour offset in fixture to fix e2e tests

--- server/fixtures/default/GetClusterFullPrediction.js
+++ server/fixtures/default/GetClusterFullPrediction.js
@@ -4,19 +4,19 @@ module.exports = {
   "result": {
     "clusterFullPrediction": {
       "clusterID": 1898714,
-      "stage3": moment().add({ days: 35, hours: 1 }).utc().format(),
+      "stage3": moment().add({ days: 35 }).utc().format(),
       "stage3Code": {
         "code": "OK",
         "description": "Prediction Successful"
       },
       "stage3Confidence": null,
-      "stage4": moment().add({ days: 70, hours: 1 }).utc().format(),
+      "stage4": moment().add({ days: 70 }).utc().format(),
       "stage4Code": {
         "code": "OK",
         "description": "Prediction Successful"
       },
       "stage4Confidence": null,
-      "stage5": moment().add({ days: 100, hours: 1 }).utc().format(),
+      "stage5": moment().add({ days: 100 }).utc().format(),
       "stage5Code": {
         "code": "OK",
         "description": "Prediction Successful"
