96a71f56b8ac142bcfdbed3aec5000cc088b1907
Chris Prokopiak <chris.prokopiak@netapp.com>
2017-03-13 14:35:12 -0600

    CLOUD-3983: Fix Policies Table

--- client/app/components/dashboard/alerts/policies/policies.service.ts
+++ client/app/components/dashboard/alerts/policies/policies.service.ts
@@ -23,13 +23,13 @@
     };
 
     let columns = [
-      {key: 'notificationName', label: 'Alert Policy Name', filterComparators: SFFilterComparators.INTEGER_DEFAULT, format: {filter: 'string'}},
-      {key: 'destinationEmail', label: 'Destination', format: {filter:'string'}},
-      {key: 'notificationSeverity', label: 'Severity', format: {filter:'string'}},
-      {key: 'username', label: 'Creator', format: {filter:'string'}},
-      {key: 'customerName', label: 'Customer', format: {filter:'string'}},
-      {key: 'clusterName', label: 'Cluster', format: {filter:'string'}},
-      {key: 'policyDescription', label: 'Alert Condition', filterComparators:SFFilterComparators.STRING_DEFAULT}
+      {key: 'notificationName', label: 'Alert Policy Name', filterComparators: SFFilterComparators.STRING_DEFAULT, format: { filter: 'string' } },
+      {key: 'destinationEmail', label: 'Destination', filterComparators: SFFilterComparators.STRING_DEFAULT, format: { filter:'string' } },
+      {key: 'notificationSeverity', label: 'Severity', filterComparators: SFFilterComparators.STRING_DEFAULT, format: { filter: 'tableBadgeAlertSeverity' } },
+      {key: 'username', label: 'Creator', filterComparators: SFFilterComparators.STRING_DEFAULT, format: { filter:'string' } },
+      {key: 'customerName', label: 'Customer', filterComparators: SFFilterComparators.STRING_DEFAULT, format: { filter:'string' } },
+      {key: 'clusterName', label: 'Cluster', filterComparators: SFFilterComparators.STRING_DEFAULT, format: { filter: 'string' } },
+      {key: 'policyDescription', label: 'Alert Condition', filterComparators:SFFilterComparators.STRING_DEFAULT, format: { filter: 'string' } }
     ];
 
     return new SFTableService(listAlerts, columns, false);
