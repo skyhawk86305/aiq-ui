1c7f7a0ee5df94eea29cab813a8376e1c351cdf3
Chris Prokopiak <chris.prokopiak@netapp.com>
2017-04-18 16:26:41 -0600

    CLOUD-3417: Add message banner
    Add message banner to the top of the page.

--- client/app/app.controller.ts
+++ client/app/app.controller.ts
@@ -1,13 +1,22 @@
+import * as _ from 'lodash';
 import { PermissionError } from './app.permissions';
 
 export class AppController {
   public showNavbar = false;
   public host: string;
   public currentPage: string;
+  public messageText: string;
+  public messageType: string;
+  public messageDate: string;
+  public displayBanner: boolean;
 
-  static $inject = ['$rootScope', 'ApiLogService', '$location'];
-  constructor(private $rootScope, private apiLogService, private $location) {
+  static $inject = ['$rootScope', 'ApiLogService', '$location', '$interval', '$http'];
+  constructor(private $rootScope, private apiLogService, private $location, private $interval, private $http) {
     this.host = $location.host();
+    this.messageText = '';
+    this.messageType = 'info';
+    this.messageDate = '';
+    this.displayBanner = false;
 
     $rootScope.$on('$routeChangeSuccess', () => {
       this.showNavbar = $location.path() !== '/login' && $location.path() !== '/reset-password';
@@ -22,6 +31,41 @@ export class AppController {
       this.showNavbar = false;
       $location.path('/login').search({url: oldUrl});
     });
+  }
+
+  $onInit() {
+    this.$interval( () => this.updateBanner(), 5 * 60 * 1000 );
+    return this.updateBanner();
+  }
+
+  private updateBanner() {
+    return this.$http
+      .get('/banner-message', {
+        cache: false,
+        headers: { 'Cache-Control': 'no-cache' }
+      })
+      .then( ({ data }) => {
+        const messageIsValid = (
+          _(data).has('message') &&
+          _(data).has('type') &&
+          _(data).has('timestamp')
+        );
+        const messageIsNew = data.timestamp !== this.messageDate;
+
+        if ( messageIsValid && messageIsNew ) {
+          this.messageText = data.message;
+          this.messageType = data.type;
+          this.messageDate = data.timestamp;
+          this.displayBanner = true;
+        }
+      })
+      .catch( err => {
+        // do nothing if the banner message request fails
+      });
+  }
+
+  private dismissBanner() {
+    this.displayBanner = false;
   }
 }
 
---
+++ test/e2e/dashboard/message-banner.spec.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const support = require('../support.js');
+const mockBackend = support.mockBackend;
+const expect = require('../support.js').expect;
+const MessageBanner = require('../page-objects/components/sf-components.po').messageBanner;
+const messageBanner = new MessageBanner('aiq-message-banner');
+
+fdescribe('The message banner', function() {
+
+  describe('with a valid message JSON', function() {
+    afterEach(function() {
+      mockBackend.disable();
+    });
+
+    it('@default should display the banner', function() {
+      mockBackend.enable(browser);
+      mockBackend.http.whenGET('/banner-message').respond( () => [200, {
+        message: "Test",
+        timestamp: "0",
+        type: "info"
+      }]);
+      browser.get('#/dashboard/overview');
+      expect(messageBanner.el.isDisplayed()).to.eventually.be.true;
+      expect(messageBanner.text.getText()).to.eventually.equal('Test');
+      expect(messageBanner.button.getText()).to.eventually.equal('Dismiss');
+    });
+
+    describe('and the dismiss button is clicked', function() {
+      //browser.driver.sleep(30000);
+      it('should not display the banner', function() {
+        mockBackend.enable(browser);
+        mockBackend.http.whenGET('/banner-message').respond( () => [200, {
+          message: "Test",
+          timestamp: "0",
+          type: "info"
+        }]);
+        browser.get('#/dashboard/overview');
+        messageBanner.button.click();
+        expect(messageBanner.el.isPresent()).to.eventually.be.false;
+      });
+    });
+
+    describe('and a type of "warning"', function() {
+      it('should display a yellow banner', function() {
+        mockBackend.enable(browser);
+        mockBackend.http.whenGET('/banner-message').respond( () => [200, {
+          message: "Test",
+          timestamp: "0",
+          type: "warning"
+        }]);
+        browser.get('#/dashboard/overview');
+        messageBanner.el.getAttribute('class').then(function(classes) {
+          console.log(classes);
+          expect(classes.indexOf('sf-message-banner-warning')).not.to.equal(-1);
+        });
+      });
+    });
+
+    describe('and a type of "error"', function() {
+      it('should display a red banner', function() {
+        mockBackend.enable(browser);
+        mockBackend.http.whenGET('/banner-message').respond( () => [200, {
+          message: "Test",
+          timestamp: "0",
+          type: "error"
+        }]);
+        browser.get('#/dashboard/overview');
+        messageBanner.el.getAttribute('class').then(function(classes) {
+          expect(classes.indexOf('sf-message-banner-error')).not.to.equal(-1);
+        });
+      });
+    })
+
+  });
+
+  describe('with an invalid message JSON', function() {
+    beforeAll(function() {
+      mockBackend.enable(browser);
+      mockBackend.http.whenGET('/banner-message').respond( () => [200, {}] );
+      browser.get('#/dashboard/overview');
+    });
+
+    afterAll(function() {
+      mockBackend.disable();
+    });
+
+    it('should not display the banner', function() {
+      expect(messageBanner.el.isPresent()).to.eventually.be.false;
+    });
+  });
+});
