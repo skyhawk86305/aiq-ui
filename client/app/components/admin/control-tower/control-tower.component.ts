ba62a6695ab0751a76e7ea502456be642c79d532
Yan Li <yan.li@netapp.com>
2017-05-25 16:24:02 -0600

    add more data and fix scroll bar problem

--- client/app/components/admin/control-tower/control-tower.component.ts
+++ client/app/components/admin/control-tower/control-tower.component.ts
@@ -15,12 +15,18 @@
         {subTitle: "Max Nodes Per Cluster", dataDisplay: "82/100"},
         {subTitle: "Avg Nodes Per Cluster", dataDisplay: "24/100"},
         {subTitle: "Standard Deviation", dataDisplay: "62%"}]},
-        {title: "Volumes in Clusters", sumNum: 16.5, description: "Total Volumes in Field", numUnit: "k", viewData: [
+      {title: "Volumes in Clusters", sumNum: 16.5, description: "Total Volumes in Field", numUnit: "k", viewData: [
         {subTitle: "Min Volumes Per Cluster", dataDisplay: "2.5k/17.5k"},
         {subTitle: "Max Volumes Per Cluster", dataDisplay: "17k/17.5k"},
         {subTitle: "Avg Volumes Per Cluster", dataDisplay: "5.5k/17.5k"},
-        {subTitle: "Standard Deviation", dataDisplay: "71%"}]}
+        {subTitle: "Standard Deviation", dataDisplay: "71%"}]},
+      {title: "Volume Size", sumNum: 2.5, description: "Current Average Volume Size", numUnit: "TB", viewData: [
+        {subTitle: "Min Volume Size Per Cluster", dataDisplay: "1.3GB/8TB"},
+        {subTitle: "Max Volume Size Per Cluster", dataDisplay: "7TB/8TB"},
+        {subTitle: "Avg Volume Size Per Cluster", dataDisplay: "2.5TB/8TB"},
+        {subTitle: "Standard Deviation", dataDisplay: "52%"}]}
     ];
+    
   }
 
   angular
