01176417a9151388587118902e873e0a7279817d
Yan Li <yan.li@netapp.com>
2017-06-06 13:34:33 -0600

    link component and service

--- client/app/components/components.module.ts
+++ client/app/components/components.module.ts
@@ -14,6 +14,16 @@ import { FindClusterComponent } from './find-cluster/find-cluster.component';
 import { VmwareAlarmsComponent } from './cluster/vmware-alarms/vmware-alarms.component';
 import { VmwareAlarmsService } from './cluster/vmware-alarms/vmware-alarms.service';
 
+import { DashbergComponent } from './admin/dashberg/dashberg.component';
+import { DashbergIOPService } from './admin/dashberg/dashberg-iop.service';
+import { DashbergBandwidthService } from './admin/dashberg/dashberg-bandwidth.service';
+import { DashbergNodeService } from './admin/dashberg/dashberg-node.service';
+import { DashbergSessionService } from './admin/dashberg/dashberg-session.service';
+import { DashbergSnapshotService } from './admin/dashberg/dashberg-snapshot.service';
+import { DashbergVolumeAccessService } from './admin/dashberg/dashberg-volume-access.service';
+import { DashbergVolumeSizeService } from './admin/dashberg/dashberg-volume-size.service';
+import { DashbergVolumeService } from './admin/dashberg/dashberg-volume.service';
+
 export const ComponentsModule = angular
   .module('aiqUi.components', [])
   .component('registerCluster', RegisterClusterComponent)
@@ -28,6 +38,16 @@ export const ComponentsModule = angular
 
   .component('vmwareAlarms', VmwareAlarmsComponent)
   .service('VmwareAlarmsService', VmwareAlarmsService)
+
+  .component('dashberg', DashbergComponent)
+  .service('DashbergIOPService', DashbergIOPService)
+  .service('DashbergBandwidthService', DashbergBandwidthService)
+  .service('DashbergNodeService', DashbergNodeService)
+  .service('DashbergSessionService', DashbergSessionService)
+  .service('DashbergSnapshotService', DashbergSnapshotService)
+  .service('DashbergVolumeAccessService', DashbergVolumeAccessService)
+  .service('DashbergVolumeSizeService', DashbergVolumeSizeService)
+  .service('DashbergVolumeService', DashbergVolumeService)
 
   .name;
 
