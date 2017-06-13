66ea95828de50f77b61c02db2b166256225458b6
Yan Li <yan.li@netapp.com>
2017-06-13 15:59:38 -0600

    change format of e2e test file

--- test/e2e/page-objects/admin/dashberg.po.js
+++ test/e2e/page-objects/admin/dashberg.po.js
@@ -1,55 +1,79 @@
-'use strict';
+'use strict',
 
 module.exports = class DashbergPage {
   constructor() {
-    this.el = element(by.css('.dashberg-page'));
-    this.nodeBox = element(by.css('.node-info-box'));
-    this.volumeBox = element(by.css('.volume-info-box'));
-    this.volumeSizeBox = element(by.css('.volume-size-info-box'));
-    this.volumeAccessBox = element(by.css('.volume-access-info-box'));
-    this.IOPBox = element(by.css('.IOP-info-box'));
-    this.bandwidthBox = element(by.css('.bandwidth-info-box'));
-    this.sessionBox = element(by.css('.session-info-box'));
-    this.snapshotBox = element(by.css('.snapshot-info-box'));
-    this.nodeDataCheckbox = element(by.id('node-data-checkbox'));
-    this.nodeCheckboxModel = element(by.model('checkboxNodeModel'));
-    this.volumeDataCheckbox = element(by.id('volume-data-checkbox'));
-    this.volumeCheckboxModel = element(by.model('checkboxVolumeModel'));
-    this.volumeSizeDataCheckbox = element(by.id('volume-size-data-checkbox'));
-    this.volumeSizeCheckboxModel = element(by.model('checkboxVolumeSizeModel'));
-    this.volumeAccessDataCheckbox = element(by.id('volume-access-data-checkbox'));
-    this.volumeAccessCheckboxModel = element(by.model('checkboxVolumeAccessModel'));
-    this.IOPDataCheckbox = element(by.id('IOP-data-checkbox'));
-    this.IOPCheckboxModel = element(by.model('checkboxIOPModel'));
-    this.bandwidthDataCheckbox = element(by.id('bandwidth-data-checkbox'));
-    this.bandwidthCheckboxModel = element(by.model('checkboxBandwidthModel'));
-    this.sessionDataCheckbox = element(by.id('session-data-checkbox'));
-    this.sessionCheckboxModel = element(by.model('checkboxSessionModel'));
-    this.snapshotDataCheckbox = element(by.id('snapshot-data-checkbox'));
-    this.snapshotCheckboxModel = element(by.model('checkboxSnapshotModel'));
-    this.nodeDataTitle = element(by.css('.node-data-title'));
-    this.nodeDataDescription = element(by.css('.node-data-description'));
-    this.nodeDataTable = element(by.css('.node-data-table'));
-    this.volumeDataTitle = element(by.css('.volume-data-title'));
-    this.volumeDataDescription = element(by.css('.volume-data-description'));
-    this.volumeDataTable = element(by.css('.volume-data-table'));
-    this.volumeSizeDataTitle = element(by.css('.volume-size-data-title'));
-    this.volumeSizeDataDescription = element(by.css('.volume-size-data-description'));
-    this.volumeSizeDataTable = element(by.css('.volume-size-data-table'));
-    this.volumeAccessDataTitle = element(by.css('.volume-access-data-title'));
-    this.volumeAccessDataDescription = element(by.css('.volume-access-data-description'));
-    this.volumeAccessDataTable = element(by.css('.volume-access-data-table'));
-    this.IOPDataTitle = element(by.css('.IOP-data-title'));
-    this.IOPDataDescription = element(by.css('.IOP-data-description'));
-    this.IOPDataTable = element(by.css('.IOP-data-table'));
-    this.bandwidthDataTitle = element(by.css('.bandwidth-data-title'));
-    this.bandwidthDataDescription = element(by.css('.bandwidth-data-description'));
-    this.bandwidthDataTable = element(by.css('.bandwidth-data-table'));
-    this.sessionDataTitle = element(by.css('.session-data-title'));
-    this.sessionDataDescription = element(by.css('.session-data-description'));
-    this.sessionDataTable = element(by.css('.session-data-table'));
-    this.snapshotDataTitle = element(by.css('.snapshot-data-title'));
-    this.snapshotDataDescription = element(by.css('.snapshot-data-description'));
-    this.snapshotDataTable = element(by.css('.snapshot-data-table'));
+    this.el = element(by.css('.dashberg-page')),
+
+    this.nodeInfoBox = {
+      nodeBox: element(by.css('.node-info-box')),
+      nodeDataTitle: element(by.css('.node-data-title')),
+      nodeDataDescription: element(by.css('.node-data-description')),
+      nodeDataTable: element(by.css('.node-data-table')),
+      nodeDataCheckbox: element(by.id('node-data-checkbox')),
+      nodeCheckboxModel: element(by.model('checkboxNodeModel'))
+    }
+
+    this.volumeInfoBox = {
+      volumeBox: element(by.css('.volume-info-box')),
+      volumeDataTitle: element(by.css('.volume-data-title')),
+      volumeDataDescription: element(by.css('.volume-data-description')),
+      volumeDataTable: element(by.css('.volume-data-table')),
+      volumeDataCheckbox: element(by.id('volume-data-checkbox')),
+      volumeCheckboxModel: element(by.model('checkboxVolumeModel'))
+    }
+
+    this.volumeSizeInfoBox = {
+      volumeSizeBox: element(by.css('.volume-size-info-box')),
+      volumeSizeDataTitle: element(by.css('.volume-size-data-title')),
+      volumeSizeDataDescription: element(by.css('.volume-size-data-description')),
+      volumeSizeDataTable: element(by.css('.volume-size-data-table')),
+      volumeSizeDataCheckbox: element(by.id('volume-size-data-checkbox')),
+      volumeSizeCheckboxModel: element(by.model('checkboxVolumeSizeModel'))
+    }
+
+    this.volumeAccessInfoBox = {
+      volumeAccessBox: element(by.css('.volume-access-info-box')),
+      volumeAccessDataTitle: element(by.css('.volume-access-data-title')),
+      volumeAccessDataDescription: element(by.css('.volume-access-data-description')),
+      volumeAccessDataTable: element(by.css('.volume-access-data-table')),
+      volumeAccessDataCheckbox: element(by.id('volume-access-data-checkbox')),
+      volumeAccessCheckboxModel: element(by.model('checkboxVolumeAccessModel'))
+    }
+
+    this.IOPInfoBox = {
+      IOPBox: element(by.css('.IOP-info-box')),
+      IOPDataTitle: element(by.css('.IOP-data-title')),
+      IOPDataDescription: element(by.css('.IOP-data-description')),
+      IOPDataTable: element(by.css('.IOP-data-table')),
+      IOPDataCheckbox: element(by.id('IOP-data-checkbox')),
+      IOPCheckboxModel: element(by.model('checkboxIOPModel'))
+    }
+
+    this.bandwidthInfoBox = {
+      bandwidthBox: element(by.css('.bandwidth-info-box')),
+      bandwidthDataTitle: element(by.css('.bandwidth-data-title')),
+      bandwidthDataDescription: element(by.css('.bandwidth-data-description')),
+      bandwidthDataTable: element(by.css('.bandwidth-data-table')),
+      bandwidthDataCheckbox: element(by.id('bandwidth-data-checkbox')),
+      bandwidthCheckboxModel: element(by.model('checkboxBandwidthModel'))
+    }
+
+    this.sessionInfoBox = {
+      sessionBox: element(by.css('.session-info-box')),
+      sessionDataTitle: element(by.css('.session-data-title')),
+      sessionDataDescription: element(by.css('.session-data-description')),
+      sessionDataTable: element(by.css('.session-data-table')),
+      sessionDataCheckbox: element(by.id('session-data-checkbox')),
+      sessionCheckboxModel: element(by.model('checkboxSessionModel'))
+    }
+
+    this.snapshotInfobox = {
+      snapshotBox: element(by.css('.snapshot-info-box')),
+      snapshotDataTitle: element(by.css('.snapshot-data-title')),
+      snapshotDataDescription: element(by.css('.snapshot-data-description')),
+      snapshotDataTable: element(by.css('.snapshot-data-table')),
+      snapshotDataCheckbox: element(by.id('snapshot-data-checkbox')),
+      snapshotCheckboxModel: element(by.model('checkboxSnapshotModel'))
+    }
   }
 }
