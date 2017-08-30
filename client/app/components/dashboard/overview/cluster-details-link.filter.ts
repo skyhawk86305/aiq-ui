54d23a15fbdbfbb5a898076e78ab1a298251a1af
greg white <gregory.white2@netapp.com>
2017-08-29 20:37:34 -0600

    Changes to fix link URL. Test on dev

--- client/app/components/dashboard/overview/cluster-details-link.filter.ts
+++ client/app/components/dashboard/overview/cluster-details-link.filter.ts
@@ -8,7 +8,7 @@
         const url = buildURL(clusterID);
         return `<a class="cluster-details-link" href="${url}">${name}</a>`;
         function buildURL(clusterID) {
-          return `#/cluster/${clusterID}`;
+          return `#/cluster/${clusterID}/`;
         }
       };
     });
