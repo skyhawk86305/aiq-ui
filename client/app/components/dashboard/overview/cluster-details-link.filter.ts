46cbbef11223e3da13bcfaf4c5a07429ea4c4484
greg white <gregory.white2@netapp.com>
2017-09-05 14:00:02 -0600

    Change single quotes to tick marks for string evaluation

--- client/app/components/dashboard/overview/cluster-details-link.filter.ts
+++ client/app/components/dashboard/overview/cluster-details-link.filter.ts
@@ -5,11 +5,7 @@
     .module('aiqUi')
     .filter('clusterDetailsLink', function() {
       return function (name, clusterID) {
-        const url = buildURL(clusterID);
-        return `<a class="cluster-details-link" href="${url}">${name}</a>`;
-        function buildURL(clusterID) {
-          return `#/cluster/${clusterID}/`;
+        return `<a class="cluster-details-link" href="#/cluster/${clusterID}/">${name}</a>`;
         }
-      };
     });
 })();
